0142e512ec1bd9a1672c1913acd9f03c787e2315
Ravi Dhiman <ravi.dhiman@tokopedia.com>
2019-12-05 15:49:07 +0530

    Basic shapes

--- src/BasicShapes/index.js
+++ src/BasicShapes/index.js
@@ -7,6 +7,7 @@ const BasicShapes = () => {
       <line x1="200" y1="200" x2="220" y2="200" stroke="blue" opacity="0.5" />
       <line x1="220" y1="200" x2="220" y2="180" stroke="blue" opacity="0.5" />
       <line x1="220" y1="180" x2="240" y2="180" stroke="blue" opacity="0.5" />
+      <path d="M10,10 C20,20 80,20 100,10" />
     </Sketch>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import BasicShapes from './BasicShapes'
-import ViewBoxZoom from './ViewBoxZoom'
+import BasicShapes from './BasicShapes'
+// import ViewBoxZoom from './ViewBoxZoom'
 // import Cardioid from './Cardioid'
 // import LangtonAnt from './LangtonAnt';
 // import Print10 from './Print10'
 import { initializeEventListener } from './helpers/events';
 
 const App = () => {
-  return <ViewBoxZoom />
+  return <BasicShapes />
 }
 
 const rootElm = document.getElementById('root');
